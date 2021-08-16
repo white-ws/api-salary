351b0fa0fdb4b26969e5e552d10cc00147e98d04
Viet Hoang <mail@viethd.work>
2021-08-16 20:52:33 +0900

    change api path

--- src/main/kotlin/com/thelonedev/plugins/API.kt
+++ src/main/kotlin/com/thelonedev/plugins/API.kt
@@ -13,17 +13,22 @@ fun Route.salaryRoutes() {
     val listSalaries: ListSalaries by inject()
     val addSalary: AddSalary by inject()
 
-    route("/salaries") {
-
-        // list
+    route("/api") {
         get {
-            call.respond(listSalaries())
+            call.respondText { "Hi!" }
         }
 
-        // create
-        post {
-            val request = call.receive<AddSalaryRequest>()
-            addSalary(request)
+        route("/salaries") {
+            // list
+            get {
+                call.respond(listSalaries())
+            }
+
+            // create
+            post {
+                val request = call.receive<AddSalaryRequest>()
+                addSalary(request)
+            }
         }
     }
 }
