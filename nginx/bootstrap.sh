70315a37eb5e9a1ea7cee7f8de2ca42ad445787e
Viet Hoang <mail@viethd.work>
2021-08-28 23:34:46 +0900

    final ssl tweak

--- nginx/bootstrap.sh
+++ nginx/bootstrap.sh
@@ -4,6 +4,6 @@ echo "PATH=$PATH" > /etc/cron.d/certbot-renew
 echo "@monthly certbot renew --nginx >> /var/log/cron.log 2>&1" >>/etc/cron.d/certbot-renew
 crontab /etc/cron.d/certbot-renew
 
-sed 's/${NGINX_HOST}/'"$NGINX_HOST"'/g' ops/ssl.conf > /etc/nginx/conf.d/ssl.conf
+sed 's/${NGINX_HOST}/'"$NGINX_HOST"'/g' /ops/ssl.conf > /etc/nginx/conf.d/ssl.conf
 
 cron && nginx -g 'daemon off;'
